9b4d15319c3efe733cca95482f4e0cc3d2cced5f
Hromada of Novopillya <nvp.hromada@gmail.com>
2019-01-08 11:38:21 +0200

    Прописали META інформацію для favicon та OpenGraph теги.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,8 +27,31 @@ JS
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="<?= Html::encode($this->title)?>"/>
+    <meta property="og:image" content="<?= \yii\helpers\Url::base(true).'/ico/apple-icon-180x180.png'?>"/>
+    <meta property="og:image:url"    content="<?= \yii\helpers\Url::base(true).'/ico/apple-icon-180x180.png'?>"/>
+    <meta property="og:image:type"   content="image/png" />
+    <meta property="og:image:width"  content="180" />
+    <meta property="og:image:height" content="180" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/ico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/ico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/ico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/ico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/ico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/ico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/ico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/ico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/ico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/ico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/ico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/ico/favicon-16x16.png">
+    <link rel="manifest" href="/ico/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ico/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <title><?= Html::encode($this->title)?></title>
     <?php $this->head() ?>
 </head>
 <body>
