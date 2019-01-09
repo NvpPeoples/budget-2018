49c50077838e838d45478782b7842c40af29cef9
Hromada of Novopillya <nvp.hromada@gmail.com>
2019-01-09 11:02:09 +0200

    Описали вимоги для проекту, внесли зміни в опис проекту

--- requirements.php
+++ requirements.php
@@ -23,28 +23,6 @@ if (!is_dir($frameworkPath)) {
 require_once($frameworkPath . '/requirements/YiiRequirementChecker.php');
 $requirementsChecker = new YiiRequirementChecker();
 
-$gdMemo = $imagickMemo = 'Either GD PHP extension with FreeType support or ImageMagick PHP extension with PNG support is required for image CAPTCHA.';
-$gdOK = $imagickOK = false;
-
-if (extension_loaded('imagick')) {
-    $imagick = new Imagick();
-    $imagickFormats = $imagick->queryFormats('PNG');
-    if (in_array('PNG', $imagickFormats)) {
-        $imagickOK = true;
-    } else {
-        $imagickMemo = 'Imagick extension should be installed with PNG support in order to be used for image CAPTCHA.';
-    }
-}
-
-if (extension_loaded('gd')) {
-    $gdInfo = gd_info();
-    if (!empty($gdInfo['FreeType Support'])) {
-        $gdOK = true;
-    } else {
-        $gdMemo = 'GD extension should be installed with FreeType support in order to be used for image CAPTCHA.';
-    }
-}
-
 /**
  * Adjust requirements according to your application specifics.
  */
@@ -64,41 +42,10 @@ $requirements = array(
         'memo' => 'Required for SQLite database.',
     ),
     array(
-        'name' => 'PDO MySQL extension',
-        'mandatory' => false,
-        'condition' => extension_loaded('pdo_mysql'),
-        'by' => 'All DB-related classes',
-        'memo' => 'Required for MySQL database.',
-    ),
-    array(
-        'name' => 'PDO PostgreSQL extension',
-        'mandatory' => false,
-        'condition' => extension_loaded('pdo_pgsql'),
-        'by' => 'All DB-related classes',
-        'memo' => 'Required for PostgreSQL database.',
-    ),
-    // Cache :
-    array(
-        'name' => 'Memcache extension',
-        'mandatory' => false,
-        'condition' => extension_loaded('memcache') || extension_loaded('memcached'),
-        'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-caching-memcache.html">MemCache</a>',
-        'memo' => extension_loaded('memcached') ? 'To use memcached set <a href="http://www.yiiframework.com/doc-2.0/yii-caching-memcache.html#$useMemcached-detail">MemCache::useMemcached</a> to <code>true</code>.' : ''
-    ),
-    // CAPTCHA:
-    array(
-        'name' => 'GD PHP extension with FreeType support',
-        'mandatory' => false,
-        'condition' => $gdOK,
-        'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-captcha-captcha.html">Captcha</a>',
-        'memo' => $gdMemo,
-    ),
-    array(
-        'name' => 'ImageMagick PHP extension with PNG support',
-        'mandatory' => false,
-        'condition' => $imagickOK,
-        'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-captcha-captcha.html">Captcha</a>',
-        'memo' => $imagickMemo,
+        'name' => 'The Collator extension',
+        'mandatory' => true,
+        'condition' => extension_loaded('intl'),
+        'by' => 'Provides string comparison capability with support for appropriate locale-sensitive sort orderings.',
     ),
     // PHP ini :
     'phpExposePhp' => array(
@@ -123,15 +70,5 @@ $requirements = array(
         'memo' => 'PHP mail SMTP server required',
     ),
 );
-
-// OPcache check
-if (!version_compare(phpversion(), '5.5', '>=')) {
-    $requirements[] = array(
-        'name' => 'APC extension',
-        'mandatory' => false,
-        'condition' => extension_loaded('apc'),
-        'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-caching-apccache.html">ApcCache</a>',
-    );
-}
 
 $requirementsChecker->checkYii()->check($requirements)->render();
